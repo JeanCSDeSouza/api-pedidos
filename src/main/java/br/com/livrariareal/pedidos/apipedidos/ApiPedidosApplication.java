c5c07cd8152395506c993b76b2189a72aba0baeb
Jean Carlos <jcsouza.first@gmail.com>
2023-01-11 02:50:32 -0300

    Código inicial

---
+++ src/main/java/br/com/livrariareal/pedidos/apipedidos/ApiPedidosApplication.java
@@ -0,0 +1,14 @@
+package br.com.livrariareal.pedidos.apipedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPedidosApplication.class, args);
+				//.run(ApiPedidosApplication.class, args);
+	}
+
+}
